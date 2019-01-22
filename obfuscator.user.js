7cc7ae41758577247bf7b81d88364ef7199c8f22
uhx <11718563+uhx@users.noreply.github.com>
2019-01-22 22:10:35 +0300

    minor (nothing changed)

--- obfuscator.user.js
+++ obfuscator.user.js
@@ -11,8 +11,8 @@
 
 (function() {
     /* chars to replace
-       cp1251 on the left side
-       ansi on the right (english) */
+       cyrillic on the left side
+       latin on the right        */
     let replacement = {
         "а" : "a",
         "А" : "A",
@@ -38,7 +38,7 @@
         return target.split(search).join(replaceto);
     };
 
-    function ObfuscateText( text )
+    function obfuscateText( text )
     {
         var obfuscated_text = "";
         var source_text = text;
@@ -78,7 +78,7 @@
                 var parts = new_text.split( regexp );
 
                 parts.forEach(function(part){
-                    new_text = new_text.replace( part, ObfuscateText(part) );
+                    new_text = new_text.replace( part, obfuscateText(part) );
                 });
 
                 im_editable.innerText = new_text;
